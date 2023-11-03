0fda2ea12b0802dff7627a316800941c766f151b
ficijj <90655281+ficijj@users.noreply.github.com>
2023-11-03 18:59:54 -0400

    added computer selection with animation and game logic (only prints results to console atm)

--- page.js
+++ page.js
@@ -1,15 +1,62 @@
+let userChoices = [document.getElementById('rock'), document.getElementById('paper'), document.getElementById('scissors')]
+let userSelected = -1;
+let compChoices = [document.getElementById('comp-rock'), document.getElementById('comp-paper'), document.getElementById('comp-scissors')]
+let compSelected = -1;
+
 function select(picked) {
-    if(picked === 'r') {
-        document.getElementById('rock').classList.add('select');
-        document.getElementById('paper').classList.remove('select');
-        document.getElementById('scissors').classList.remove('select');
-    } else if(picked === 'p') {
-        document.getElementById('rock').classList.remove('select');
-        document.getElementById('paper').classList.add('select');
-        document.getElementById('scissors').classList.remove('select');
-    } else if(picked === 's') {
-        document.getElementById('rock').classList.remove('select');
-        document.getElementById('paper').classList.remove('select');
-        document.getElementById('scissors').classList.add('select');
+    for(let i = 0; i < userChoices.length; i++) {
+        if(picked === userChoices[i]) {
+            userChoices[i].classList.add('select');
+            userSelected = i;
+        } else {
+            userChoices[i].classList.remove('select');
+        }
+    }
+}
+
+function compPick() {
+    document.getElementById('question').classList.add('hide');
+    compChoices[0].classList.remove('hide');
+    let rand = Math.ceil(Math.random() * 2000 + 3000);
+    console.log(rand);
+    let i = 0;
+    var tid = setInterval(function() {
+        compChoices[i % 3].classList.add('hide');
+        compChoices[(i + 1) % 3].classList.remove('hide');
+        i++;
+    }, 125);
+    setTimeout(function() {
+        clearInterval(tid);
+        compSelected = i % 3;
+        determineWinLoss();
+    }, rand);
+}
+
+function determineWinLoss() {
+    switch(true) {
+        default:
+            console.log('NOT SELECTED');
+            break;
+        case (userSelected === compSelected):  // Both picked the same
+            console.log('Tie!');
+            break;
+        case (userSelected === 0 && compSelected === 1):  // User picked rock, computer picked paper
+            console.log('Loss!');
+            break;
+        case (userSelected === 0 && compSelected === 2):  // User picked rock, computer picked scissors
+            console.log('Win!');
+            break;
+        case (userSelected === 1 && compSelected === 0):  // User picked paper, computer picked rock
+            console.log('Win!');
+            break;
+        case (userSelected === 1 && compSelected === 2):  // User picked paper, computer picked scissors
+            console.log('Loss!');
+            break;
+        case (userSelected === 2 && compSelected === 0):  // User picked scissors, computer picked rock
+            console.log('Loss!');
+            break;
+        case (userSelected === 2 && compSelected === 1):  // User picked scissors, computer picked paper
+            console.log('win!');
+            break;
     }
 }
